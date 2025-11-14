839bb7dcdb017a504ad1231ce3d3685526581f42
rismanitalst <rismanitalst@gmail.com>
2025-11-14 21:03:08 +0700

    ganti font style jadi roboto dan dancing script

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,53 @@
 import 'package:flutter/material.dart';
+
 void main() => runApp(const MyApp());
+
 class MyApp extends StatelessWidget {
-const MyApp({super.key});
-@override
-Widget build(BuildContext context) {
-return MaterialApp(
-title: 'Custom Fonts',
-// Set Raleway as the default app font.
-theme: ThemeData(fontFamily: 'Raleway'),
-home: const MyHomePage(),
-);
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Custom Fonts',
+      // Set Raleway as the default app font.
+      theme: ThemeData(fontFamily: 'Raleway'),
+      home: const MyHomePage(),
+    );
+  }
 }
-}
+
 class MyHomePage extends StatelessWidget {
-const MyHomePage({super.key});
-@override
-Widget build(BuildContext context) {
+  const MyHomePage({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Custom Fonts')),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Hello Rismanita!",
+              style: TextStyle(
+                fontFamily: "roboto",
+                fontStyle: FontStyle.italic,
+                fontSize: 22,
+              ),
+            ),
 
-return Scaffold(
-// The AppBar uses the app-default Raleway font.
-appBar: AppBar(title: const Text('Custom Fonts')),
-body: const Center(
-// This Text widget uses the RobotoMono font.
-child: Text(
-'Roboto Mono sample',
-style: TextStyle(fontFamily: 'RobotoMono'),
-),
-),
-);
-}
+            SizedBox(height: 8), // jarak antar teks
+
+            Text(
+              "Ini aku gi latihan pake font style yuhuuuuu",
+              style: TextStyle(
+                fontFamily: "dancing", 
+                fontWeight: FontWeight.w900
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
